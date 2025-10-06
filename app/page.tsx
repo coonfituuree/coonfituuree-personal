fe63b78ac61969d0f6758e5ffe59c3aa6cf8d395
gala <galymbartai@gmail.com>
2025-10-06 22:55:48 +0500

    fixing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to the Coonfituuree's Page</h1>
+      <h1 className="text-4xl font-bold">Welcome to the Coonfituuree&apos;s Page</h1>
     </main>
   );
 }

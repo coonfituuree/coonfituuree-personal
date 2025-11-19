c9b352307551092e5763606e1663149d5a22cc10
Galym <galymbartai@gmail.com>
2025-11-19 09:20:40 +0500

    new

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Welcome to the Coonfituuree&apos;s Page</h1>
+      <h1>https://disk.yandex.kz/d/L6zZY7ysKqcy6A</h1>
     </main>
   );
 }
